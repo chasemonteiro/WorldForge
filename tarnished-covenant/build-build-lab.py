44b414d08fe0235eb4e8e5c63eada1cd67d54fd2
chasemonteiro <chasemonteiro@gmail.com>
2026-09-22 20:52:34 -0600

    Keep Build lab multiplayer cache version current

--- tarnished-covenant/build-build-lab.py
+++ tarnished-covenant/build-build-lab.py
@@ -5,7 +5,7 @@ path = Path(__file__).with_name("index.html")
 html = path.read_text()
 
 css_tag = '<link rel="stylesheet" href="./build-lab.css?v=6">'
-js_tag = '<script src="./build-lab.js?v=7"></script>'
+js_tag = '<script src="./build-lab.js?v=8"></script>'
 
 if css_tag not in html:
     if "</head>" not in html:
