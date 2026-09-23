c0c65d75751e5f9400328b82036be88fbeab28dc
chasemonteiro <chasemonteiro@gmail.com>
2026-09-23 09:00:09 -0600

    Advance hardened Build Lab cache versions

--- tarnished-covenant/build-build-lab.py
+++ tarnished-covenant/build-build-lab.py
@@ -4,8 +4,8 @@ import re
 path = Path(__file__).with_name("index.html")
 html = path.read_text()
 
-css_tag = '<link rel="stylesheet" href="./build-lab.css?v=7">'
-js_tag = '<script src="./build-lab.js?v=12"></script>'
+css_tag = '<link rel="stylesheet" href="./build-lab.css?v=8">'
+js_tag = '<script src="./build-lab.js?v=13"></script>'
 
 css_pattern = r'<link rel="stylesheet" href="\./build-lab\.css\?v=\d+">'
 js_pattern = r'<script src="\./build-lab\.js\?v=\d+"></script>'
